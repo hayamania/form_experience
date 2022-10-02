295531702088eb471be175029a2a5a1216c213e4
Chie Hym <hayama.chie@gmail.com>
2022-10-02 14:53:38 +0800

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to Chie HYM club. Please fill your information to register.</p>
+    </div>
+  );
+}
+
+export default App;
