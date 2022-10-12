67c88967f9b39d34af5149069c9b7f1a6ad0c1c9
Chie Hym <hayama.chie@gmail.com>
2022-10-12 19:00:04 +0800

    set default value

--- src/component/FormMain.js
+++ src/component/FormMain.js
@@ -1,16 +1,51 @@
 import React, { useState } from "react";
 import { Box, Button, Stack, Stepper, Step, StepLabel } from "@mui/material";
+import { useForm, FormProvider } from "react-hook-form";
 import Form1 from "./Form1";
 import Form2 from "./Form2";
 import Form3 from "./Form3";
 
 export default function FormMain() {
+  const methods = useForm();
   const [step, setStep] = useState(0);
   const [titles, setTitles] = useState([
     "Personal Information",
     "Professional Information",
     "Confirm your input",
   ]);
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      emailc: "",
+      password: "",
+      passwordc: "",
+      mobile: "",
+      phone: "",
+      dob: "",
+      gender: "",
+      presentPosition: "",
+      qualification1: "",
+      qualification2: "",
+      skills: {
+        HTML: false,
+        CSS: false,
+        Bootstrap: false,
+        React: false,
+        SASS: false,
+        Angler: false,
+        MySQL: false,
+        PHP: false,
+        Python: false,
+        Ruby: false,
+        C: false,
+        Java: false,
+      },
+      agree: false,
+    },
+  });
+  const onSubmit = (data) => console.log(data);
   const ShowForm = () => {
     switch (step) {
       case 0:
@@ -27,7 +62,7 @@ export default function FormMain() {
         justifyContent="center"
         alignItems="center"
         sx={{
-          "& > :not(style)": { m: 1, width: "600px" },
+          "& > :not(style)": { m: 2, p: 2, width: "600px" },
         }}
       >
         <h1>Welcome to Chie HYM club. </h1>
@@ -44,33 +79,39 @@ export default function FormMain() {
           </Stepper>
         </div>
         <div>{titles[step]}</div>
-        {ShowForm()}
-        <div>
-          <Stack
-            direction="row"
-            justifyContent="space-evenly"
-            alignItems="center"
-          >
-            <Button
-              onClick={() => {
-                setStep((previouse) => previouse - 1);
-              }}
-              variant="outlined"
-              disabled={step === 0 ? true : false}
-            >
-              Back
-            </Button>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setStep((previouse) => previouse + 1);
-              }}
-              disabled={step === titles.length - 1 ? true : false}
-            >
-              {step === titles.length - 1 ? "Submit" : "Next"}
-            </Button>
-          </Stack>
-        </div>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {ShowForm()}
+            <div>
+              <Stack
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+                sx={{ mt: 2 }}
+              >
+                <Button
+                  onClick={() => {
+                    setStep((previouse) => previouse - 1);
+                  }}
+                  variant="outlined"
+                  disabled={step === 0 ? true : false}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    step === titles.length - 1
+                      ? alert("Thanks for your submission.")
+                      : setStep((previouse) => previouse + 1);
+                  }}
+                >
+                  {step === titles.length - 1 ? "Submit" : "Next"}
+                </Button>
+              </Stack>
+            </div>
+          </form>
+        </FormProvider>
       </Box>
     </div>
   );
